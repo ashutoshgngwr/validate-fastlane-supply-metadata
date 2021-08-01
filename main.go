3dab4ddc6954ed5beea1db7cd08a92433d3eea0f
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2021-08-01 22:44:05 +0530

    refactor(action): clean up code

--- main.go
+++ main.go
@@ -22,12 +22,29 @@ type imageConfig struct {
 	format string
 }
 
+type validationError struct {
+	File string
+	Err  error
+}
+
+var _ error = &validationError{}
+
+func (e *validationError) Error() string {
+	return fmt.Sprintf("%s: %s", e.File, e.Err.Error())
+}
+
+func (e *validationError) annotateGitHubFile() {
+	const errAnnotationFmt = "::error file=%s::%s\n"
+	v := strings.ReplaceAll(e.Err.Error(), "%", "%25")
+	v = strings.ReplaceAll(v, "\r", "%0D")
+	v = strings.ReplaceAll(v, "\n", "%0A")
+
+	fmt.Printf(errAnnotationFmt, e.File, v)
+}
+
 var (
 	fastlanePath        string
 	enableGAAnnotations bool
-
-	// nErrors keeps the count of failed checks during a single run.
-	nErrors uint
 )
 
 func init() {
@@ -40,182 +57,217 @@ func main() {
 	metadataPath := filepath.Join(fastlanePath, "metadata", "android")
 	files, err := ioutil.ReadDir(metadataPath)
 	if err != nil {
-		fmt.Printf("Unable to list metadata directory: %s\n", err.Error())
+		const errFmt = "failed to read directory %q: %s\n"
+		fmt.Fprintf(os.Stderr, errFmt, metadataPath, err)
 		os.Exit(1)
 	}
 
+	errs := make([]error, 0)
 	for _, f := range files {
 		if !f.IsDir() {
 			// we are only interested in directories
 			continue
 		}
 
-		processDescriptiveTexts(metadataPath, f.Name())
-		processImages(metadataPath, f.Name())
-		processChangelogs(metadataPath, f.Name())
+		localePath := filepath.Join(metadataPath, f.Name())
+		imagesPath := filepath.Join(localePath, "images")
+		changelogsPath := filepath.Join(localePath, "changelogs")
+		errs = append(errs, checkDescriptiveTexts(localePath)...)
+		errs = append(errs, checkImages(imagesPath)...)
+		errs = append(errs, checkChangelogs(changelogsPath)...)
 	}
 
-	if nErrors > 0 {
-		fmt.Fprintf(os.Stderr, "%d total checks failing!\n", nErrors)
+	fmt.Println("found", len(errs), "errors!")
+	for _, err := range errs {
+		if ve, ok := err.(*validationError); ok && enableGAAnnotations {
+			ve.annotateGitHubFile()
+		}
+
+		fmt.Fprintln(os.Stderr, err.Error())
+	}
+
+	if len(errs) > 0 {
 		os.Exit(1)
 	}
 }
 
-// processDescriptiveTexts will check *.txt files in metadata. updates the count
-// of nErrors for failed checks. also prints all the errors for failing checks
-// and annonates corresponding files.
-func processDescriptiveTexts(basePath, locale string) {
-	const errLengthExceededFmt = "content length exceeded the limit! expected: %d, got: %d"
+// checkDescriptiveTexts checks *.txt files in metadata. It returns a slice of
+// `error` with all IO and validation errors.
+func checkDescriptiveTexts(localePath string) []error {
 	descriptiveFileLengths := map[string]int{
 		"title.txt":             50,
 		"short_description.txt": 80,
 		"full_description.txt":  4000,
 	}
 
+	errs := make([]error, 0)
 	for file, length := range descriptiveFileLengths {
-		file = filepath.Join(basePath, locale, file)
+		file = filepath.Join(localePath, file)
 		count, err := getCharacterCount(file)
-		if err == nil && count > length {
-			err = fmt.Errorf(errLengthExceededFmt, length, count)
-		}
-
 		if err != nil {
-			nErrors++
-			annotateFileWithError(file, err)
-			logError(locale, filepath.Base(file), err)
+			const errFmt = "failed to read file %q: %w"
+			errs = append(errs, fmt.Errorf(errFmt, file, err))
+		} else if count > length {
+			const errFmt = "content length exceeded: expected=%d, got=%d"
+			errs = append(errs, &validationError{
+				File: file,
+				Err:  fmt.Errorf(errFmt, length, count),
+			})
 		}
 	}
+
+	return errs
 }
 
 // getCharacterCount counts the utf-8 characters in the given file.
 func getCharacterCount(filePath string) (int, error) {
 	content, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		return 0, fmt.Errorf("error reading the file: %s", err.Error())
+		return 0, err
 	}
 
 	return utf8.RuneCountInString(strings.TrimSpace(string(content))), nil
 }
 
-// processImages process image assets in images/* including screenshots. updates
-// the count of nErrors for failed checks. also prints all the errors for
-// failing checks and annonates corresponding files.
-func processImages(basePath, locale string) {
-	imagesBasePath := filepath.Join(basePath, locale, "images")
-	files, err := ioutil.ReadDir(imagesBasePath)
-	if err != nil && !os.IsNotExist(err) { // ignore not found errors since this is an optional dir
-		nErrors++
-		logError(locale, "images", err)
-		return
+// checkImages checks image assets in `images/*` including screenshots. It
+// returns a slice of `error` with all IO and validation errors.
+func checkImages(imagesPath string) []error {
+	files, err := ioutil.ReadDir(imagesPath)
+	// since directory is optional, ignore 'not exist' errors.
+	if err != nil && !os.IsNotExist(err) {
+		const errFmt = "failed to read directory %q: %w"
+		return []error{fmt.Errorf(errFmt, imagesPath, err)}
 	}
 
+	errs := make([]error, 0)
 	for _, file := range files {
 		if file.IsDir() {
 			if strings.HasSuffix(file.Name(), "Screenshots") {
-				processScreenshotImages(basePath, locale, file.Name())
-			}
-		} else {
-			filePath := filepath.Join(imagesBasePath, file.Name())
-			fileName := strings.TrimSuffix(filepath.Base(file.Name()), filepath.Ext(file.Name()))
-			config, err := getImageConfig(filePath)
-			if err != nil {
-				nErrors++
-				err = fmt.Errorf("error while reading image: %s", err)
-				annotateFileWithError(filePath, err)
-				logError(locale, fileName, err)
-				continue
+				errs = append(errs, checkScreenshots(filepath.Join(imagesPath, file.Name()))...)
 			}
 
-			errs := make([]error, 0)
-			switch fileName {
-			case "icon":
-				if config.width != config.height || config.width != 512 {
-					errs = append(errs, fmt.Errorf("must be 512x512"))
-				}
-				if config.format != "png" {
-					errs = append(errs, fmt.Errorf("must be a PNG"))
-				}
-				break
-			case "featureGraphic":
-				if config.width != 1024 || config.height != 500 {
-					errs = append(errs, fmt.Errorf("must be 1024x500"))
-				}
-				if !config.opaque {
-					errs = append(errs, fmt.Errorf("must be opaque"))
-				}
-				break
-			case "promoGraphic":
-				if config.width != 180 || config.height != 120 {
-					errs = append(errs, fmt.Errorf("must be 180x120"))
-				}
-				if !config.opaque {
-					errs = append(errs, fmt.Errorf("must be opaque"))
-				}
-				break
-			case "tvBanner":
-				if config.width != 1280 || config.height != 720 {
-					errs = append(errs, fmt.Errorf("must be 1280x720"))
-				}
-				if !config.opaque {
-					errs = append(errs, fmt.Errorf("must be opaque"))
-				}
-				break
-			}
+			continue
+		}
 
-			for _, err = range errs {
-				nErrors++
-				annotateFileWithError(filePath, err)
-				logError(locale, fileName, err)
+		filePath := filepath.Join(imagesPath, file.Name())
+		config, err := getImageConfig(filePath)
+		if err != nil {
+			const errFmt = "failed to read image %q: %w"
+			errs = append(errs, fmt.Errorf(errFmt, filePath, err))
+			continue
+		}
+
+		switch name := strings.TrimSuffix(filepath.Base(file.Name()), filepath.Ext(file.Name())); name {
+		case "icon":
+			if config.width != config.height || config.width != 512 {
+				const errFmt = "icon must be 512x512: got=%dx%d"
+				errs = append(errs, &validationError{
+					File: filePath,
+					Err:  fmt.Errorf(errFmt, config.width, config.height),
+				})
+			}
+			if config.format != "png" {
+				errs = append(errs, &validationError{
+					File: filePath,
+					Err:  fmt.Errorf("icon must be a PNG"),
+				})
+			}
+		case "featureGraphic":
+			if config.width != 1024 || config.height != 500 {
+				const errFmt = "featureGraphic must be 1024x500: got=%dx%d"
+				errs = append(errs, &validationError{
+					File: filePath,
+					Err:  fmt.Errorf(errFmt, config.width, config.height),
+				})
+			}
+			if !config.opaque {
+				errs = append(errs, &validationError{
+					File: filePath,
+					Err:  fmt.Errorf("featureGraphic must be opaque"),
+				})
+			}
+		case "promoGraphic":
+			if config.width != 180 || config.height != 120 {
+				const errFmt = "promoGraphic must be 180x120: got=%dx%d"
+				errs = append(errs, &validationError{
+					File: filePath,
+					Err:  fmt.Errorf(errFmt, config.width, config.height),
+				})
+			}
+			if !config.opaque {
+				errs = append(errs, &validationError{
+					File: filePath,
+					Err:  fmt.Errorf("promoGraphic must be opaque"),
+				})
+			}
+		case "tvBanner":
+			if config.width != 1280 || config.height != 720 {
+				const errFmt = "tvBanner must be 1280x720: got=%dx%d"
+				errs = append(errs, &validationError{
+					File: filePath,
+					Err:  fmt.Errorf(errFmt, config.width, config.height),
+				})
+			}
+			if !config.opaque {
+				errs = append(errs, &validationError{
+					File: filePath,
+					Err:  fmt.Errorf("tvBanner must be opaque"),
+				})
 			}
 		}
 	}
+
+	return errs
 }
 
-// processScreenshotImages checks all the screenshot images. updates the count
-// of nErrors for failed checks. also prints all the errors for failing checks
-// and annonates corresponding files.
-func processScreenshotImages(basePath, locale, dirName string) {
-	path := filepath.Join(basePath, locale, "images", dirName)
-	files, err := ioutil.ReadDir(path)
+// checkScreenshots checks all screenshot images. It returns a slice of `error`
+// with all IO and validation errors.
+func checkScreenshots(screenshotsPath string) []error {
+	files, err := ioutil.ReadDir(screenshotsPath)
 	if err != nil {
-		nErrors++
-		logError(locale, dirName, err)
-		return
+		const errFmt = "failed to read directory %q: %w"
+		return []error{fmt.Errorf(errFmt, screenshotsPath, err)}
 	}
 
+	errs := make([]error, 0)
 	for _, file := range files {
-		filePath := filepath.Join(path, file.Name())
-		config, err := getImageConfig(filePath)
+		imagePath := filepath.Join(screenshotsPath, file.Name())
+		config, err := getImageConfig(imagePath)
 		if err != nil {
-			nErrors++
-			annotateFileWithError(filePath, err)
-			logError(locale, filepath.Join(dirName, file.Name()), err)
+			const errFmt = "failed to read image %q: %w"
+			errs = append(errs, fmt.Errorf(errFmt, imagePath, err))
 			continue
 		}
 
 		if config.width < 320 || config.width > 3840 {
-			nErrors++
-			err = fmt.Errorf("width should be at least 320px and at most 3840px")
-			annotateFileWithError(filePath, err)
-			logError(locale, filepath.Join(dirName, file.Name()), err)
+			const errFmt = "width should be in range 320px-3840px: got=%dpx"
+			errs = append(errs, &validationError{
+				File: imagePath,
+				Err:  fmt.Errorf(errFmt, config.width),
+			})
 		}
 
 		if config.height < 320 || config.height > 3840 {
-			nErrors++
-			err = fmt.Errorf("height should be at least 320px and at most 3840px")
-			annotateFileWithError(filePath, err)
-			logError(locale, filepath.Join(dirName, file.Name()), err)
+			const errFmt = "height should be in range 320px-3840px: got=%dpx"
+			errs = append(errs, &validationError{
+				File: imagePath,
+				Err:  fmt.Errorf(errFmt, config.height),
+			})
 		}
 
 		width := float64(config.width)
 		height := float64(config.height)
-		if math.Max(width, height)/math.Min(height, width) > 2.0 {
-			nErrors++
-			err = fmt.Errorf("aspect ratio of max edge to min edge should be at most 2")
-			annotateFileWithError(filePath, err)
-			logError(locale, filepath.Join(dirName, file.Name()), err)
+		ratio := math.Max(width, height) / math.Min(height, width)
+		if ratio > 2.0 {
+			const errFmt = "'max:min' edge radio should be at most 2.0: got=%.2f"
+			errs = append(errs, &validationError{
+				File: imagePath,
+				Err:  fmt.Errorf(errFmt, ratio),
+			})
 		}
 	}
+
+	return errs
 }
 
 // getImageConfig returns imageConfig for the given image file. returns an error
@@ -245,7 +297,7 @@ func getImageConfig(filePath string) (*imageConfig, error) {
 		if oimage, ok := image.(interface{ Opaque() bool }); ok {
 			opaque = oimage.Opaque()
 		} else {
-			return nil, fmt.Errorf("unable to determine if image is opaque")
+			return nil, fmt.Errorf("failed to determine if image is opaque")
 		}
 	}
 
@@ -257,55 +309,38 @@ func getImageConfig(filePath string) (*imageConfig, error) {
 	}, nil
 }
 
-// processDescriptiveTexts will check changelogs/*.txt files in metadata.
-// updates the count of nErrors for failed checks. also prints all the errors
-// for failing checks and annonates corresponding files.
-func processChangelogs(basePath, locale string) {
-	const maxContentLength = 500
-	const changelogsDirName = "changelogs"
-	const errLengthExceededFmt = "content length exceeded the limit! expected: %d, got: %d"
-
-	changelogPath := filepath.Join(basePath, locale, changelogsDirName)
-	files, err := ioutil.ReadDir(changelogPath)
+// checkChangelogs checks `changelogs/*.txt` files in metadata. It returns a
+// slice of `error` containing both IO and validation errors.
+func checkChangelogs(changelogsPath string) []error {
+	files, err := ioutil.ReadDir(changelogsPath)
+	// since directory is optional, ignore 'not exist' errors.
 	if err != nil && !os.IsNotExist(err) {
-		// ignore not found errors since this is an optional dir
-		nErrors++
-		logError(locale, changelogsDirName, err)
-		return
+		const errFmt = "failed to read directory %q: %w"
+		return []error{fmt.Errorf(errFmt, changelogsPath, err)}
 	}
+
+	errs := make([]error, 0)
 	for _, file := range files {
 		if file.IsDir() {
 			continue // not expecting one.. but okay...?
 		}
 
-		filePath := filepath.Join(changelogPath, file.Name())
+		filePath := filepath.Join(changelogsPath, file.Name())
 		count, err := getCharacterCount(filePath)
-		if err == nil && count > maxContentLength {
-			err = fmt.Errorf(errLengthExceededFmt, maxContentLength, count)
-		}
-
 		if err != nil {
-			nErrors++
-			annotateFileWithError(filePath, err)
-			logError(locale, filepath.Join(changelogsDirName, file.Name()), err)
+			const errFmt = "failed to read file %q: %w"
+			errs = append(errs, fmt.Errorf(errFmt, filePath, err))
+		}
+
+		const maxContentLength = 500
+		if count > maxContentLength {
+			const errFmt = "content length exceeded: expected=%d, got=%d"
+			errs = append(errs, &validationError{
+				File: filePath,
+				Err:  fmt.Errorf(errFmt, maxContentLength, count),
+			})
 		}
 	}
-}
 
-// logError logs the error
-func logError(locale, file string, err error) {
-	fmt.Fprintf(os.Stderr, "%s/%s: %s\n", locale, file, err.Error())
-}
-
-func annotateFileWithError(filePath string, err error) {
-	if !enableGAAnnotations {
-		return
-	}
-
-	const errAnnotationFmt = "::error file=%s::%s\n"
-	v := strings.ReplaceAll(err.Error(), "%", "%25")
-	v = strings.ReplaceAll(v, "\r", "%0D")
-	v = strings.ReplaceAll(v, "\n", "%0A")
-
-	fmt.Printf(errAnnotationFmt, filePath, v)
+	return errs
 }
